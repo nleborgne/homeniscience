5e582b4d5406fa4469b3ced78105569711aec6f2
Cyril Mazarin <cyril.mazarin@isep.fr>
2018-05-04 18:08:27 +0200

    modifié ajout utilisateur

--- Ajout-utilisateur/ajout_utilisateur.php
+++ Ajout-utilisateur/ajout_utilisateur.php
@@ -19,8 +19,8 @@ catch(Exception $e)
 {
     die('Erreur : '.$e->getMessage());
 }
-<<<<<<< HEAD
 $recherche=' ';
+$mail='';
 ?>
 <div class="formulaire">
     <form class="add_capt" method="post">
@@ -36,46 +36,45 @@ $recherche=' ';
         }
         ?>
     </form>
-    <form>
-        <select class="selection" id="piece" name="piece" required>
+    <form method="POST">
+        <select class="selection" id="piece" name="user" required>
 
             <?php
-            $reponse_ajout = $bdd->query("SELECT nom,prenom FROM utilisateur WHERE nom='".$recherche."'  ");
+            $reponse_ajout = $bdd->query("SELECT email FROM utilisateur WHERE nom='".$recherche."'  ");
             while ($donnees_utilisateurs = $reponse_ajout->fetch()){
                 ?>
-                <option value="<?php echo $donnees_utilisateurs['nom'] ; echo  $donnees_utilisateurs['prenom']; ?>"> <?php echo $donnees_utilisateurs['nom']; echo $donnees_utilisateurs['prenom']; ?> </option>
-=======
-$recherche='meynet';
-?>
-<div class="formulaire">
-    <form class="add_capt" method="post">
-        <input class="champ" type="text" id="nom" name="nom" placeholder="Nom equipement" required>
-        <select class="selection" id="piece" name="piece" required>
-
-            <?php
-            if(isset($_POST['nom'])){
-                $recherche=$_POST('nom');
-            }
-
-            $reponse_ajout = $bdd->query("SELECT nom,prenom FROM utilisateur WHERE nom='".$recherche."'  ");
-            while ($donnees_utilisateurs = $reponse_ajout->fetch()){
-                ?>
-                <option value="<?php echo $donnees_utilisateurs['nom']; echo $donnees_utilisateurs['prenom']; ?>"> <?php echo $donnees_utilisateurs['nom']; echo $donnees_utilisateurs['prenom']; ?> </option>
->>>>>>> c8283bfbfc8532ad8a34eacfa35dbfd0bd2e9a42
+                <option value="<?php echo $donnees_utilisateurs['email'] ; ?>"> <?php echo $donnees_utilisateurs['email']; ?> </option>
                 <?php
             }
             ?>
         </select>
-        <select class="selection" id="invité" name="invité" required>
-            <?php $reponse_utilisateurs = $bdd->query("SELECT prenom FROM utilisateur WHERE ID_domicile=0 ORDER BY ID");
-            while ($utilisateurs = $reponse_utilisateurs->fetch()){
-                ?>
-                <option value="<?php echo $utilisateurs['prenom']; ?>"> <?php echo $utilisateurs['prenom']; ?> </option>
-                <?php
-            }
-            ?>
-        </select>
-        <input class="boutton" type="submit" value="Valider">
+
+        <input class="boutton" type="submit" name="Valider" value="Valider">
+
+        <?php
+        if(isset($_POST['Valider'])) {
+                $mail= $_POST['user'];
+                $sql = "UPDATE utilisateur SET ID_domicile=1 WHERE email=:email";
+                $stmt = $bdd->prepare($sql);
+                $stmt->execute(array(
+                        'email' => $mail
+                ));
+
+         }
+        ?>
+
     </form>
+
+
+    <select class="selection2" id="invité" name="invité" required>
+        <?php $reponse_utilisateurs = $bdd->query("SELECT prenom FROM utilisateur WHERE ID_domicile=1 ORDER BY ID");
+        while ($utilisateurs = $reponse_utilisateurs->fetch()){
+            ?>
+            <option value="<?php echo $utilisateurs['prenom']; ?>"> <?php echo $utilisateurs['prenom']; ?> </option>
+            <?php
+        }
+        ?>
+    </select>
+
 </div>
 </body>
