4a32cb43eca9e0062d12532edd9dd3c6eb2e3e68
Cyril Mazarin <cyril.mazarin@isep.fr>
2018-05-09 10:08:35 +0200

    message

--- page/domicile/domicile.php
+++ page/domicile/domicile.php
@@ -8,7 +8,7 @@
     <link rel="stylesheet" href="domicile2.0.css">
     <title>MON DOMICILE</title>
 </head>
-<?php require ('header.php'); ?>
+<?php require ('../header.php'); ?>
 <body>
 <div class="container1" >
     <div class="container1">
@@ -41,7 +41,7 @@
 
 </body>
 <footer style="position: inherit">
-
+    <?php /*require ('../../footer.php')*/?>
 </footer>
 
 </html>
