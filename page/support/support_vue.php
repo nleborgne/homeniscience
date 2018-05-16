7e0127921805346b10cd11536f32c3ba4928448e
Nicolas Le Borgne <nleborgne@isep.fr>
2018-05-16 16:50:31 +0200

    changes support

--- page/support/support_vue.php
+++ page/support/support_vue.php
@@ -3,7 +3,7 @@
 <html lang="fr" dir="ltr">
 <head>
   <meta charset="utf-8">
-  <title></title>
+  <title>Support</title>
   <link href="https://fonts.googleapis.com/css?family=Comfortaa|Lato|Roboto" rel="stylesheet">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css" integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
 
@@ -45,14 +45,12 @@
 
         <?php
         while ($donnees = $reponse->fetch()) {
-          echo '<a href="/homeniscience/page/support/panne/index/'.$donnees['ID'].'#detail">';
           echo '<ul class="listepanne">';
           echo '<li>'.$donnees['nom_equipement'].'</li> ';
           echo '<li>'.$donnees['date_panne'].'</li> ';
           echo '<li>'.$donnees['nom_type'].'</li> ';
           echo '<li>'.$donnees['descriptif_panne'].'</li> ';
           echo '</ul>';
-          echo '</a>';
         }
         ?>
       </article>
