87afc090da209068cc485a5fedb3df4f94a0aac4
Nicolas Le Borgne <nleborgne@isep.fr>
2018-05-16 09:59:40 +0200

    header modifié

--- page/header.php
+++ page/header.php
@@ -20,111 +20,116 @@ $name = getNom()->fetch();?>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
   <style>
-  body {margin:0;font-family:'Lato';}
-
-  .topnav {
-    overflow: hidden;
-    background:linear-gradient(-90deg, #4650E2, #00A2E8);
+  body {
+    margin:0;
+    font-family:'Lato';
+    background: url('/homeniscience/page/img/funky-lines.png');
+    background-attachment: fixed;
   }
 
-  .topnav a {
-    float: left;
-    display: block;
-    color: #f2f2f2;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-    font-size: 17px;
-  }
+    .topnav {
+      overflow: hidden;
+      background:linear-gradient(-90deg, #4650E2, #00A2E8);
+    }
 
-  .active {
-    background-color: #343434;
-    color: white;
-  }
+    .topnav a {
+      float: left;
+      display: block;
+      color: #f2f2f2;
+      text-align: center;
+      padding: 14px 16px;
+      text-decoration: none;
+      font-size: 17px;
+    }
 
-  .topnav .icon {
-    display: none;
-  }
+    .active {
+      background-color: #343434;
+      color: white;
+    }
 
-  .dropdown {
-    float: left;
-    overflow: hidden;
-  }
-
-  .dropdown .dropbtn {
-    font-size: 17px;
-    border: none;
-    outline: none;
-    color: white;
-    padding: 14px 16px;
-    background-color: inherit;
-    font-family: inherit;
-    margin: 0;
-  }
-
-  .dropdown-content {
-    display: none;
-    position: absolute;
-    background-color: #f9f9f9;
-    min-width: 160px;
-    box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
-    z-index: 1;
-  }
-
-  .dropdown-content a {
-    float: none;
-    color: black;
-    padding: 12px 16px;
-    text-decoration: none;
-    display: block;
-    text-align: left;
-  }
-
-  .topnav a:hover, .dropdown:hover .dropbtn {
-    background-color: #696969;
-    color: white;
-  }
-
-  .dropdown-content a:hover {
-    background-color: #ddd;
-    color: black;
-  }
-
-  .dropdown:hover .dropdown-content {
-    display: block;
-  }
-
-  @media screen and (max-width: 600px) {
-    .topnav a:not(:first-child), .dropdown .dropbtn {
+    .topnav .icon {
       display: none;
     }
-    .topnav a.icon {
-      float: right;
-      display: block;
-    }
-  }
 
-  @media screen and (max-width: 600px) {
-    .topnav.responsive {position: relative;}
-    .topnav.responsive .icon {
+    .dropdown {
+      float: left;
+      overflow: hidden;
+    }
+
+    .dropdown .dropbtn {
+      font-size: 17px;
+      border: none;
+      outline: none;
+      color: white;
+      padding: 14px 16px;
+      background-color: inherit;
+      font-family: inherit;
+      margin: 0;
+    }
+
+    .dropdown-content {
+      display: none;
       position: absolute;
-      right: 0;
-      top: 0;
+      background-color: #f9f9f9;
+      min-width: 160px;
+      box-shadow: 0px 8px 16px 0px rgba(0,0,0,0.2);
+      z-index: 1;
     }
-    .topnav.responsive a {
+
+    .dropdown-content a {
       float: none;
+      color: black;
+      padding: 12px 16px;
+      text-decoration: none;
       display: block;
       text-align: left;
     }
-    .topnav.responsive .dropdown {float: none;}
-    .topnav.responsive .dropdown-content {position: relative;}
-    .topnav.responsive .dropdown .dropbtn {
-      display: block;
-      width: 100%;
-      text-align: left;
+
+    .topnav a:hover, .dropdown:hover .dropbtn {
+      background-color: #696969;
+      color: white;
     }
-  }
-</style>
+
+    .dropdown-content a:hover {
+      background-color: #ddd;
+      color: black;
+    }
+
+    .dropdown:hover .dropdown-content {
+      display: block;
+    }
+
+    @media screen and (max-width: 600px) {
+      .topnav a:not(:first-child), .dropdown .dropbtn {
+        display: none;
+      }
+      .topnav a.icon {
+        float: right;
+        display: block;
+      }
+    }
+
+    @media screen and (max-width: 600px) {
+      .topnav.responsive {position: relative;}
+      .topnav.responsive .icon {
+        position: absolute;
+        right: 0;
+        top: 0;
+      }
+      .topnav.responsive a {
+        float: none;
+        display: block;
+        text-align: left;
+      }
+      .topnav.responsive .dropdown {float: none;}
+      .topnav.responsive .dropdown-content {position: relative;}
+      .topnav.responsive .dropdown .dropbtn {
+        display: block;
+        width: 100%;
+        text-align: left;
+      }
+    }
+  </style>
 </head>
 <body>
 
