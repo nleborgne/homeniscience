bde6d2d81e15279acfe0d3177299736a4df25de8
Nicolas Le Borgne <nleborgne@isep.fr>
2018-05-22 10:31:16 +0200

    search bar accueil

--- page/accueil/accueil_vue.php
+++ page/accueil/accueil_vue.php
@@ -5,6 +5,7 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.10/css/all.css"
   integrity="sha384-+d0P83n9kaQMCwj8F4RJB66tzIwOKmrdb46+porD/OvrJ+37WqIM7UoBtwHO6Nlg" crossorigin="anonymous">
+  <script type="text/javascript" src="https://raw.githubusercontent.com/vdw/HideSeek/master/jquery.hideseek.min.js"></script>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width">
   <title>Accueil</title>
@@ -14,6 +15,7 @@
 <body>
   <h1 class="title">Bonjour <?php echo $name['prenom'].' '.$name['nom'];?></h1>
   <div class="container">
+    <input class="search" id="search" name="search" placeholder="Start typing here" type="text" data-list=".list">
     <div class="flex2">
       <button class="accordion" type="button" name="button">Domicile</button>
       <div class="panel">
@@ -72,25 +74,26 @@
   </div>
 
   <script type="text/javascript">
-      /*
+  /*
+  old header, not used anymore (confirm in 2.1 ?)
   var acc = document.getElementsByClassName("accordion");
   //var cont = document.getElementById("topContainer");
   var i;
 
   for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function () {
-      this.classList.toggle("active");
-      var panel = this.nextElementSibling;
-      if (panel.style.maxHeight) {
-        panel.style.maxHeight = null;
-        //cont.style.maxHeight = null;
-      } else {
-        panel.style.maxHeight = panel.scrollHeight + "px";
-        //  cont.style.maxHeight = panel.scrollHeight + "px";
-      }
-    });
-  }*/
+  acc[i].addEventListener("click", function () {
+  this.classList.toggle("active");
+  var panel = this.nextElementSibling;
+  if (panel.style.maxHeight) {
+  panel.style.maxHeight = null;
+  //cont.style.maxHeight = null;
+} else {
+panel.style.maxHeight = panel.scrollHeight + "px";
+//  cont.style.maxHeight = panel.scrollHeight + "px";
+}
+});
+}*/
 </script>
 </body>
-<?php require('../footer.php') ?>
+<?php //require('../footer.php') ?>
 </html>
--- page/accueil/vue_domicile.php
+++ page/accueil/vue_domicile.php
@@ -1,30 +1,37 @@
-<?php
-while ($piece = $afficherPieces -> fetch() ) {
-  echo '<div class="child">';
-  /* On affiche la pièce */
-  echo '<h3>'.$piece['piece_nom'].'</h3>';
-  $afficherEffecteurs = getEffecteurs($piece['piece_ID']);
-  while($effecteur = $afficherEffecteurs->fetch()) {
-    echo "<div class='blockEffecteur'>";
-    switch ($effecteur['ID_type_equipement']) {
-      case 1:
-      echo '<i class="fas fa-tint fa-2x"></i>&nbsp;';
-      echo '10%';
-      break;
-      case 2:
-      echo '<i class="fas fa-lightbulb fa-2x"></i>';
-      echo '<input type="range" name="" value="">';
-      break;
-      case 3:
-      echo '<i class="fas fa-child fa-2x"></i>&nbsp;';
-      echo 'nobody detected';
-      break;
+<table id="table" align="left">
+  <?php
+  while ($piece = $afficherPieces -> fetch() ) {
+    echo '<tr>';
+    echo '<td>';
+    echo '<div class="child">';
+    /* On affiche la pièce */
+    echo '<h3>'.$piece['piece_nom'].'</h3>';
+    $afficherEffecteurs = getEffecteurs($piece['piece_ID']);
+    while($effecteur = $afficherEffecteurs->fetch()) {
+      echo "<div class='blockEffecteur'>";
+      switch ($effecteur['ID_type_equipement']) {
+        case 1:
+        echo '<i class="fas fa-tint fa-2x"></i>&nbsp;';
+        echo '10%';
+        break;
+        case 2:
+        echo '<i class="fas fa-lightbulb fa-2x"></i>';
+        echo '<input type="range" name="" value="">';
+        break;
+        case 3:
+        echo '<i class="fas fa-child fa-2x"></i>&nbsp;';
+        echo 'nobody detected';
+        break;
+      }
+      echo '</div>';
     }
     echo '</div>';
+  //  echo '</td>';
+    //echo '</tr>';
   }
-  echo '</div>';
-}
-
+  ?>
+</table>
+<?php
 /* Tenter d'afficher les effecteurs d'une pièce
 echo '<i class="fas fa-lightbulb fa-2x"></i>';
 echo '<input type="range" name="" value="">';
@@ -37,6 +44,17 @@ echo '<input type="range" name="" value="">';*/
 
 ?>
 
+<script type="text/javascript">
+var $rows = $('#table tr');
+$('#search').keyup(function() {
+    var val = $.trim($(this).val()).replace(/ +/g, ' ').toLowerCase();
+
+    $rows.show().filter(function() {
+        var text = $(this).text().replace(/\s+/g, ' ').toLowerCase();
+        return !~text.indexOf(val);
+    }).hide();
+});
+</script>
 <style media="screen">
 .child {
   margin-top:20px;
@@ -53,91 +71,91 @@ echo '<input type="range" name="" value="">';*/
 }
 .blockEffecteur {
   height:45px;
-}input[type=range] {
-  -webkit-appearance: none;
-  width: 90%;
-  margin: 7.3px 0;
-}
-input[type=range]:focus {
-  outline: none;
-}
-input[type=range]::-webkit-slider-runnable-track {
-  width: 100%;
-  height: 11.4px;
-  cursor: pointer;
-  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-  background: rgba(0, 102, 235, 0.78);
-  border-radius: 1.3px;
-  border: 0.2px solid #010101;
-}
-input[type=range]::-webkit-slider-thumb {
-  box-shadow: 0.9px 0.9px 1px #000031, 0px 0px 0.9px #00004b;
-  border: 1.8px solid #00001e;
-  height: 26px;
-  width: 26px;
-  border-radius: 15px;
-  background: #ffffff;
-  cursor: pointer;
-  -webkit-appearance: none;
-  margin-top: -7.5px;
-}
-input[type=range]:focus::-webkit-slider-runnable-track {
-  background: rgba(5, 114, 255, 0.78);
-}
-input[type=range]::-moz-range-track {
-  width: 100%;
-  height: 11.4px;
-  cursor: pointer;
-  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-  background: rgba(0, 102, 235, 0.78);
-  border-radius: 1.3px;
-  border: 0.2px solid #010101;
-}
-input[type=range]::-moz-range-thumb {
-  box-shadow: 0.9px 0.9px 1px #000031, 0px 0px 0.9px #00004b;
-  border: 1.8px solid #00001e;
-  height: 26px;
-  width: 26px;
-  border-radius: 15px;
-  background: #ffffff;
-  cursor: pointer;
-}
-input[type=range]::-ms-track {
-  width: 100%;
-  height: 11.4px;
-  cursor: pointer;
-  background: transparent;
-  border-color: transparent;
-  color: transparent;
-}
-input[type=range]::-ms-fill-lower {
-  background: rgba(0, 91, 210, 0.78);
-  border: 0.2px solid #010101;
-  border-radius: 2.6px;
-  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-}
-input[type=range]::-ms-fill-upper {
-  background: rgba(0, 102, 235, 0.78);
-  border: 0.2px solid #010101;
-  border-radius: 2.6px;
-  box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
-}
-input[type=range]::-ms-thumb {
-  box-shadow: 0.9px 0.9px 1px #000031, 0px 0px 0.9px #00004b;
-  border: 1.8px solid #00001e;
-  height: 26px;
-  width: 26px;
-  border-radius: 15px;
-  background: #ffffff;
-  cursor: pointer;
-  height: 11.4px;
-}
-input[type=range]:focus::-ms-fill-lower {
-  background: rgba(0, 102, 235, 0.78);
-}
-input[type=range]:focus::-ms-fill-upper {
-  background: rgba(5, 114, 255, 0.78);
-}
+  }input[type=range] {
+    -webkit-appearance: none;
+    width: 90%;
+    margin: 7.3px 0;
+  }
+  input[type=range]:focus {
+    outline: none;
+  }
+  input[type=range]::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 11.4px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: rgba(0, 102, 235, 0.78);
+    border-radius: 1.3px;
+    border: 0.2px solid #010101;
+  }
+  input[type=range]::-webkit-slider-thumb {
+    box-shadow: 0.9px 0.9px 1px #000031, 0px 0px 0.9px #00004b;
+    border: 1.8px solid #00001e;
+    height: 26px;
+    width: 26px;
+    border-radius: 15px;
+    background: #ffffff;
+    cursor: pointer;
+    -webkit-appearance: none;
+    margin-top: -7.5px;
+  }
+  input[type=range]:focus::-webkit-slider-runnable-track {
+    background: rgba(5, 114, 255, 0.78);
+  }
+  input[type=range]::-moz-range-track {
+    width: 100%;
+    height: 11.4px;
+    cursor: pointer;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+    background: rgba(0, 102, 235, 0.78);
+    border-radius: 1.3px;
+    border: 0.2px solid #010101;
+  }
+  input[type=range]::-moz-range-thumb {
+    box-shadow: 0.9px 0.9px 1px #000031, 0px 0px 0.9px #00004b;
+    border: 1.8px solid #00001e;
+    height: 26px;
+    width: 26px;
+    border-radius: 15px;
+    background: #ffffff;
+    cursor: pointer;
+  }
+  input[type=range]::-ms-track {
+    width: 100%;
+    height: 11.4px;
+    cursor: pointer;
+    background: transparent;
+    border-color: transparent;
+    color: transparent;
+  }
+  input[type=range]::-ms-fill-lower {
+    background: rgba(0, 91, 210, 0.78);
+    border: 0.2px solid #010101;
+    border-radius: 2.6px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  input[type=range]::-ms-fill-upper {
+    background: rgba(0, 102, 235, 0.78);
+    border: 0.2px solid #010101;
+    border-radius: 2.6px;
+    box-shadow: 1px 1px 1px #000000, 0px 0px 1px #0d0d0d;
+  }
+  input[type=range]::-ms-thumb {
+    box-shadow: 0.9px 0.9px 1px #000031, 0px 0px 0.9px #00004b;
+    border: 1.8px solid #00001e;
+    height: 26px;
+    width: 26px;
+    border-radius: 15px;
+    background: #ffffff;
+    cursor: pointer;
+    height: 11.4px;
+  }
+  input[type=range]:focus::-ms-fill-lower {
+    background: rgba(0, 102, 235, 0.78);
+  }
+  input[type=range]:focus::-ms-fill-upper {
+    background: rgba(5, 114, 255, 0.78);
+  }
 
 
 </style>
