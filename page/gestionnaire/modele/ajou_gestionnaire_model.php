b5ca9a35abc22a70788215f2a238d2c758ae062c
Guillaume Meynet <guillaume.meynet@isep.fr>
2018-06-01 09:50:35 +0200

    g

--- page/gestionnaire/modele/ajou_gestionnaire_model.php
+++ page/gestionnaire/modele/ajou_gestionnaire_model.php
@@ -7,26 +7,48 @@ catch(Exception $e) {
     die('Erreur : '.$e->getMessage());
 }
 
-function ajout_domicile($post, $ID_type_habitation, $ID_gestionnaire){
+function ajout_domicile($ID_type_habitation, $ID_gestionnaire){
     global $bdd;
-    global $_SESSION;
     $ID_confidentialite=0;
     $consommation = 0;
     $req = $bdd->prepare("INSERT INTO domicile (ID_utilisateur_principal, nom, nombre_pieces, superficie, ID_type_habitation, numero_habitation, rue, code_postal, pays, ID_confidentialite, ID_gestionnaire, consommation)
                                     VALUES (:ID_utilisateur_principal, :nom, :nombre_pieces, :superficie, :ID_type_habitation, :numero_habitation, :rue, :code_postal, :pays, :ID_confidentialite, :ID_gestionnaire, :consommation)");
     $req->bindParam('ID_utilisateur_principal',$_SESSION['ID'],PDO::PARAM_INT);
-    $req->bindParam('nom',$_POST['nom'],PDO::PARAM_STR);
-    $req->bindParam('nombre_pieces',$post['nbre_piece'],PDO::PARAM_INT);
-    $req->bindParam('superficie',$post['superficie'],PDO::PARAM_INT);
+    $req->bindParam('nom',$_POST['nom_domicile'],PDO::PARAM_STR);
+    $req->bindParam('nombre_pieces',$_POST['nbre_piece'],PDO::PARAM_INT);
+    $req->bindParam('superficie',$_POST['superficie'],PDO::PARAM_INT);
     $req->bindParam('ID_type_habitation',$ID_type_habitation,PDO::PARAM_INT);
-    $req->bindParam('numero_habitation ',$post['numero'],PDO::PARAM_INT);
-    $req->bindParam('rue',$post['rue'],PDO::PARAM_INT);
-    $req->bindParam('code_postal',$post['postal'],PDO::PARAM_INT);
-    $req->bindParam('pays',$post['pays'],PDO::PARAM_STR);
+    $req->bindParam('numero_habitation ',$_POST['numero'],PDO::PARAM_INT);
+    $req->bindParam('rue',$_POST['rue'],PDO::PARAM_STR);
+    $req->bindParam('code_postal',$_POST['postal'],PDO::PARAM_INT);
+    $req->bindParam('pays',$_POST['pays'],PDO::PARAM_STR);
     $req->bindParam('ID_confidentialite',$ID_confidentialite,PDO::PARAM_INT);
     $req->bindParam('ID_gestionnaire',$ID_gestionnaire,PDO::PARAM_INT);
     $req->bindParam('consommation',$consommation,PDO::PARAM_INT);
     $req->execute();
+}
+
+function Adddomicile($ID_utilisateur_principal,$nom,$size,$num,$rue,$cp,$pays)
+{
+    global $bdd;
+    $requete = $bdd ->prepare('INSERT INTO domicile(ID,ID_utilisateur_principal,nom,nombre_pieces,superficie,ID_type_habitation,numero_habitation,rue,code_postal,pays,ID_confidentialite,ID_gestionnaire, consommation)
+                                VALUES (:ID,:ID_utilisateur_principal,:nom,:nombre_pieces,:superficie,:ID_type_habitation,:numero_habitation,:rue,:code_postal,:pays,:ID_confidentialite,:ID_gestionnaire, :conso)');
+    $requete ->execute(array(
+        'ID' =>NULL,
+        'ID_utilisateur_principal' => $ID_utilisateur_principal,
+        'nom' => $nom,
+        'nombre_pieces' => 0,
+        'superficie' =>$size,
+        'ID_type_habitation' =>1,
+        'numero_habitation' =>$num,
+        'rue' =>$rue,
+        'code_postal' =>$cp,
+        'pays' =>$pays,
+        'ID_confidentialite' =>1,
+        'ID_gestionnaire'=>0,
+        'conso'=>0,
+    ));
+
 }
 
 function AfficherDomicile()
@@ -51,6 +73,6 @@ function get_right_gest(){
 
 function get_type_habitation(){
     global $bdd;
-    $req= $bdd->query("SELECT nom_type FROM type_habitation ORDER BY ID");
+    $req= $bdd->query("SELECT * FROM type_habitation");
     return $req;
 }
