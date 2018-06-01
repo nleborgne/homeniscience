28fbe166dff34a04c6136642bd0d195cf0b721eb
Nicolas Le Borgne <nleborgne@isep.fr>
2018-06-01 10:12:26 +0200

    trames

--- Trame.php
+++ Trame.php
@@ -29,15 +29,38 @@ Class Trame
         $this->trame = $trame;
     }
 
-    public function type_capteur() {
+    public function getTypeCapteur() {
         return substr($this->trame,6,1);
     }
-
-    public function numero_capteur() {
+    public function getNumeroCapteur() {
         return substr($this->trame,7,2);
     }
-
     public function getValeur() {
-        return substr($this->trame,8,3);
+        return substr($this->trame,9,4);
     }
+    public function getNumeroTrame() {
+        return substr($this->trame,13,4);
+    }
+    public function getChecksum() {
+        return substr($this->trame,17,2);
+    }
+    public function getAnnee() {
+        return substr($this->trame,19,4);
+    }
+    public function getMois() {
+        return substr($this->trame,23,2);
+    }
+    public function getJour() {
+        return substr($this->trame,25,2);
+    }
+    public function getHeure() {
+        return substr($this->trame,27,2);
+    }
+    public function getMinute() {
+        return substr($this->trame,29,2);
+    }
+    public function getSeconde() {
+        return substr($this->trame,31,2);
+    }
+
 }
--- trames_traitement.php
+++ trames_traitement.php
@@ -1,4 +1,8 @@
 <?php
+
+// PAGE DE TEST DES TRAMES
+// PAS DE MVC IL SERA AJOUTÉ PAR LA SUITE
+
 require('Trame.php');
 
 $ch = curl_init();
@@ -11,26 +15,43 @@ curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
 $data = curl_exec($ch);
 curl_close($ch);
 
-$newArray = str_split($data,33);
+$newArray = str_split($data, 33);
 
 $file = "trames.txt";
 $fopen = fopen($file, 'r');
-$fread = fread($fopen,filesize($file));
+$fread = fread($fopen, filesize($file));
 fclose($fopen);
 
 // Tableau de trames
 $trameArray = array();
 
 // PATTERN : "1009C"
-for($i = 0; $i < strlen($fread); $i++){
-    if (substr($fread,$i,5) == "1009C") {
-        array_push($trameArray,new Trame(substr($fread,$i,33)));
+for ($i = 0; $i < strlen($fread); $i++) {
+    if (substr($fread, $i, 5) == "1009C") {
+        array_push($trameArray, new Trame(substr($fread, $i, 33)));
     }
 }
 
-
-echo $trameArray[0]->type_capteur();
-echo " ";
-echo $trameArray[0]->numero_capteur();
-echo " ";
-echo $trameArray[0]->getValeur();
+try {
+    $bdd = new PDO('mysql:host=localhost;dbname=homeniscience;charset=utf8', 'root', '');
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
+foreach ($trameArray as $trame) {
+    $get = $bdd->prepare('SELECT * from statistiques WHERE trame = ?');
+    $get->execute(array($trame->getTrame()));
+    $count = $get->rowCount();
+    if ($count > 0) {
+        echo "<p>déjà dans BDD </p>";
+    } else {
+        $insert = $bdd->prepare('INSERT INTO statistiques(ID_equipement,ID_type_equipement,date,donnee,trame) VALUES(:ID_equipement,:ID_type_equipement,:date,:donnee,:trame)');
+        $insert->execute(array(
+            'ID_equipement' => $trame->getNumeroCapteur(),
+            'ID_type_equipement' => $trame->getTypeCapteur(),
+            'date' => $trame->getAnnee() . "-" . $trame->getMois() . "-" . $trame->getJour() . "-" . $trame->getHeure() . "-" . $trame->getMinute() . "-" . $trame->getSeconde(),
+            'donnee' => $trame->getValeur(),
+            'trame' => $trame->getTrame()
+        ));
+        echo "<p>Requête ajoutée</p>";
+    }
+}
