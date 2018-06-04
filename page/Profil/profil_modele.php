3abe50092110f258f9520f838e74f1cc1518e188
Victor Kolopp <vic.kolopp@gmail.com>
2018-06-04 09:56:56 +0200

    zyra

--- page/Profil/profil_modele.php
+++ page/Profil/profil_modele.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 try
 {
     $bdd = new PDO('mysql:host=localhost;dbname=homeniscience;charset=utf8', 'root', '');
@@ -10,48 +8,44 @@ catch(Exception $e) {
     die('Erreur : ' . $e->getMessage());
 }
 
-function getUtilisateur($id_user) {
+function getUtilisateur($idUser) {
     global $bdd;
     $req = $bdd->prepare('SELECT * FROM utilisateur WHERE ID = :id');
-    $req->bindParam('id', $id_user, PDO::PARAM_INT);
+    $req->bindParam('id', $idUser, PDO::PARAM_INT);
     $req->execute();
     return $req;
 }
 
-function modifNomUtilisateur($nom,$prenom,$id_user) {
+function modifNomUtilisateur($nom,$prenom,$idUser) {
     global $bdd;
     $req = $bdd->prepare('UPDATE utilisateur SET nom = :nvnom, prenom = :nvprenom WHERE ID = :id');
-    $req->execute( array(
-        'nvnom' => $nom,
-        'nvprenom' => $prenom,
-        'id' => $id_user
-    ));
+    $req->bindParam('nvnom',$nom,PDO::PARAM_STR);
+    $req->bindParam('nvprenom',$prenom,PDO::PARAM_STR);
+    $req->bindParam('id',$idUser,PDO::PARAM_INT);
+    $req->execute();
 }
 
-function modifEmailUtilisateur($email, $id_user){
+function modifEmailUtilisateur($email, $idUser){
     global $bdd;
     $req = $bdd->prepare('UPDATE utilisateur SET email = :nvemail WHERE ID = :id');
-    $req->execute( array(
-        'nvemail' => $email,
-        'id' => $id_user
-    ));
+    $req->bindParam('nvemail',$email,PDO::PARAM_STR);
+    $req->bindParam('id',$idUser,PDO::PARAM_INT);
+    $req->execute();
 }
 
-function getMdpUtilisateur($id_user){
+function getMdpUtilisateur($idUser){
     global $bdd;
     $req = $bdd->prepare('SELECT mot_de_passe FROM utilisateur WHERE ID = :id');
-    $req->bindParam('id', $id_user, PDO::PARAM_INT);
+    $req->bindParam('id', $idUser, PDO::PARAM_INT);
     $req->execute();
     return $req;
 }
 
-function modifMdpUtilisateur($mdp, $id_user){
+function modifMdpUtilisateur($mdp, $idUser){
     global $bdd;
     $req = $bdd->prepare('UPDATE utilisateur SET mot_de_passe = :mdp WHERE ID = :id');
-    $req->execute( array(
-        'mdp' => $mdp,
-        'id' => $id_user
-    ));
+    $req->bindParam('mdp', $mdp, PDO::PARAM_STR);
+    $req->bindParam('id', $idUser, PDO::PARAM_INT);
 }
 
 
