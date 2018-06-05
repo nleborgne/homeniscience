30ad70466fd7de30d072a86d839caf441d47abe9
Guillaume Meynet <guillaume.meynet@isep.fr>
2018-06-05 10:15:37 +0200

    g

--- connexion/vue_connexion.php
+++ connexion/vue_connexion.php
@@ -9,26 +9,9 @@
 </head>
 <body>
 <div class="bg">
-<<<<<<< HEAD
-  <img class="imgHome" src="img/logo.png" alt="">
   <!-- Module de connexion -->
 
-      <div class="connexion">
-        <form method="POST" action="traitement_connexion.php">
-          <input class="txtinput" type="text" name="email" placeholder="Email">
-          <br>
-          <input class="txtinput" type="password" name="mot_de_passe" placeholder="Mot de passe">
-          <input class="btn" type="submit">
-        </form>
-      </div>
-      <!-- Créer un compte / mdp oublié -->
-      <div class="links">
-        <a href="">Mot de passe oublié ?</a>
-        <a href="../inscription/Inscription.php">Créer un compte</a>
-      </div>
-  </div>
-=======
-    <img class="imgHome" src="img/logo.png" alt="">
+       <img class="imgHome" src="img/logo.png" alt="">
     <!-- Module de connexion -->
 
         <div class="connexion">
@@ -45,6 +28,6 @@
         <a href="../inscription/Inscription.php">Créer un compte</a>
     </div>
 </div>
->>>>>>> 4e9ea15fc020a2a0e69aa879f96e5cb26836021a
+  </div>
 </body>
 </html>
