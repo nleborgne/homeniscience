a8e9a0e0cc92a0872e528229d402039bbd47e142
Ludovic <ludovic.martinet@isep.fr>
2018-06-05 09:35:24 +0100

    yo

--- page/gestionnaire/modele/appart_modele.php
+++ page/gestionnaire/modele/appart_modele.php
@@ -15,3 +15,16 @@ function AfficherDomicile2() {
     $req->execute(array($_GET['id']));
     return $req;
 }
+
+/* fonction pour ajouter un utilisateur de la bdd pour le gestionnaire */
+function AjouterUser($id_dom, $email) {
+    global $bdd;
+    $req = $bdd->prepare('UPDATE utilisateur SET ID_domicile = :id_dom WHERE email = :email');
+    $req -> bindParam('id_dom', $id_dom, PDO::PARAM_INT);
+    $req -> bindParam('email', $email, PDO::PARAM_STR);
+    $req->execute();
+}
+
+
+
+
