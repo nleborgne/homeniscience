eadce1950a0ed817e8473cde3e81aa392f16cd13
Ludovic <ludovic.martinet@isep.fr>
2018-06-06 14:19:38 +0100

    yo

--- page/gestionnaire/modele/appart_modele.php
+++ page/gestionnaire/modele/appart_modele.php
@@ -16,14 +16,38 @@ function AfficherDomicile2() {
     return $req;
 }
 
-/* fonction pour ajouter un utilisateur de la bdd pour le gestionnaire */
+/* fonction pour ajouter un utilisateur de la bdd pour le gestionnaire (modif utilisateur ID_domicile)
+ * et modif type_user en principal */
 function AjouterUser($id_dom, $email) {
     global $bdd;
-    $req = $bdd->prepare('UPDATE utilisateur SET ID_domicile = :id_dom WHERE email = :email');
+    $req = $bdd->prepare('UPDATE utilisateur SET ID_domicile = :id_dom, ID_type_utilisateur = 2 WHERE email = :email');
     $req -> bindParam('id_dom', $id_dom, PDO::PARAM_INT);
     $req -> bindParam('email', $email, PDO::PARAM_STR);
     $req->execute();
+    header('Refresh:0');
 }
+
+
+/* fonction pour recuperer l'ID d'un utilisateur suivant son email */
+function getUser($email) {
+    global $bdd;
+    $req = $bdd -> prepare('SELECT ID FROM utilisateur WHERE email = :email');
+    $req -> bindParam('email', $email, PDO::PARAM_STR);
+    $req -> execute(); 
+    return $req;
+}
+
+/* fonction pour supprimer un utilisateur en tant que gestionnaire */
+function supprUser($id_user) {
+    global $bdd;
+    $req = $bdd -> prepare('UPDATE utilisateur SET ID_domicile = 0 WHERE ID = :id_user');
+    $req -> bindParam('id_user', $id_user, PDO::PARAM_INT);
+    $req -> execute();   
+    header('Refresh:0');
+}
+
+
+
 
 
 
