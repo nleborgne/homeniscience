7d1c8964ef60b06b971767ae1f4babe666089771
Cyril Mazarin <cyrildu972@gmail.com>
2018-06-08 11:33:07 +0200

    ACCEUIL

--- page/domicile/ajout_capteur.php
+++ page/domicile/ajout_capteur.php
@@ -46,8 +46,8 @@ if(!isset($_SESSION)){
     <div class="formulaire">
         <form class="add_cEMAC" action="index.php" method="post">
             <h4>Ajouter un CeMac</h4>
-            <input class="champ" type="text" id="numero" name="numero" placeholder="numero CeMac" required>
-            <select class="select-style" id="piece" name="piece" required>
+            <input class="champ" type="text" id="numero" name="numero" placeholder="numero CeMac" required style="width: 280px;  margin: 5px ">
+            <select class="select-style" id="piece" name="piece" required style="width: 280px;  margin: 5px ">
 
                 <option value="NULL"> Sélectionnez une piece </option>
                 <?php
@@ -61,14 +61,14 @@ if(!isset($_SESSION)){
 
             <br>
 
-            <input class="boutton" name="addc" type="submit" value="Valider">
+            <input class="boutton" name="addc" type="submit" value="Valider" style="  margin: 5px ">
 
         </form>
     </div>
         <div class="formulaire">
         <form method="POST" action="index.php">
             <h4>Supprimer un CeMac</h4>
-            <select class="select-style" id="piece" name="ID_cem" required>
+            <select class="select-style" id="piece" name="ID_cem" required >
                 <option value="NULL"> Sélectionnez un CeMac </option>
                 <?php
 
@@ -91,8 +91,8 @@ if(!isset($_SESSION)){
     <div class="formulaire">
         <form class="add_capt" method="post">
             <h4>Ajouter un capteur</h4>
-            <input class="champ" type="text" id="nom" name="nom" placeholder="Nom equipement" required>
-            <select class="select-style" id="piece" name="piece" required>
+            <input class="champ" type="text" id="nom" name="nom" placeholder="Nom equipement" required style="width: 280px;  margin: 5px ">
+            <select class="select-style" id="piece" name="piece" required style="width: 280px;  margin: 5px ">
                 <option value="NULL"> Sélectionnez une piece </option>
                 <?php
                 while ($donnees_piece = $reponse_piece->fetch()){
@@ -102,7 +102,7 @@ if(!isset($_SESSION)){
                 }
                 ?>
             </select>
-            <select class="select-style" id="type_capteur" name="type_capteur" required>
+            <select class="select-style" id="type_capteur" name="type_capteur" required style="width: 280px;  margin: 5px ">
                 <option value="NULL"> Type de capteur </option>
                 <?php
                 while ($donnees_capteur = $reponse_capteur->fetch()){
@@ -113,7 +113,7 @@ if(!isset($_SESSION)){
                 ?>
             </select>
             <br>
-            <input class="boutton" name="add" type="submit" value="Valider">
+            <input class="boutton" name="add" type="submit" value="Valider" style="  margin: 5px ">
 
         </form>
     </div>
@@ -131,13 +131,14 @@ if(!isset($_SESSION)){
                 ?>
                 <input class="boutton" type="submit" name="dell" value="Supprimer" >
         </form>
-    </div>
-    </article>
         <?php
         while ($equ_dom = $equipement_ajoutés->fetch()){
-            ?><p style="/*box-shadow: 2px 2px 5px rgba(0, 0, 0, .1);*/">
+        ?><p style="/*box-shadow: 2px 2px 5px rgba(0, 0, 0, .1);*/">
             <?php echo $equ_dom['nom']; ?>
             <?php
-        }
-        ?>
+            }
+            ?>
+    </div>
+    </article>
+
 </body>
