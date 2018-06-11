c55cf6f374c1495919591ea6b016b89eaf974376
Nicolas Le Borgne <nleborgne@isep.fr>
2018-06-11 14:31:20 +0200

    verif client mdp js

--- connexion/controleur_oubli.php
+++ connexion/controleur_oubli.php
@@ -68,8 +68,8 @@ function sendMail($code, $user)
         $mail->isSMTP();                                      // Set mailer to use SMTP
         $mail->Host = 'smtp.gmail.com';  // Specify main and backup SMTP servers
         $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = '';                 // SMTP username
-        $mail->Password = '';                           // SMTP password
+        $mail->Username = 'xlink78@gmail.com';                 // SMTP username
+        $mail->Password = 'Sexiondassaut';                           // SMTP password
         $mail->SMTPSecure = 'tls';                            // Enable TLS encryption, `ssl` also accepted
         $mail->Port = 587;                                    // TCP port to connect to
 
--- connexion/vue_oubli_nv_mdp.php
+++ connexion/vue_oubli_nv_mdp.php
@@ -15,7 +15,7 @@
             <h2 class="profil_modifier">Nouveau mot de passe</h2>
             <p>Choisissez un nouveau mot de passe.<br> Il doit comporter au moins 8 caractères, une minuscule, une
                 majuscule et un chiffre.</p>
-            <form method="GET" action="controleur_oubli.php">
+            <form method="GET" action="controleur_oubli.php" onsubmit="return verifMdp()">
                 <fieldset>
                     <legend> Entrez votre nouveau mot de passe</legend>
                     <div class="row">
@@ -36,16 +36,56 @@
                     </div>
                 </fieldset>
                 <label for="mail3">
-                    <input type="text" name="mail3" value="<?php echo $_GET['mail2']?> " style="display:none" >
+                    <input type="text" name="mail3" value="<?php echo $_GET['mail2'] ?> " style="display:none">
                 </label>
                 <div class="row">
                     <input type="submit" value="Envoyer le code">
                 </div>
             </form>
         </div>
+        <div id="error"></div>
     </div>
 </div>
+<script type="text/javascript">
+    function verifMdp() {
 
+        var anUpperCase = /[A-Z]/;
+        var aLowerCase = /[a-z]/;
+        var aNumber = /[0-9]/;
+        var numUpper = 0;
+        var numLower = 0;
+        var numNums = 0;
+
+        var pass = document.getElementById("mdp").value;
+        var pass2 = document.getElementById("mdp2").value;
+        var message = document.getElementById("error");
+
+        if (pass !== pass2) {
+            message.innerHTML = "<p>Les mots de passe ne correspondent pas</p>"
+            return false;
+        } else {
+            if (pass.value.length < 8) {
+                message.innerHTML = "<p>Mot de passe pas assez long</p>";
+                return false;
+            }
+            for (var i = 0; i < pass.length; i++) {
+                if (anUpperCase.test(pass[i]))
+                    numUpper++;
+                else if (aLowerCase.test(pass[i]))
+                    numLower++;
+                else if (aNumber.test(pass[i]))
+                    numNums++;
+            }
+            if (numUpper < 1 || numLower < 1 || numNums < 1 ) {
+                message.innerHTML = "<p>Le mot de passe doit contenir une minuscule, une majuscule et un chiffre</p>";
+                return false;
+            } else {
+                return true;
+            }
+        }
+    }
+
+</script>
 </body>
 <footer style="position: inherit">
 </footer>
