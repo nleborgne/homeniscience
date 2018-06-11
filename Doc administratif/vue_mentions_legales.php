a070084ffd36a67aa4340bbfef540c979dec7257
Triskhell <ksinant.tristan@gmail.com>
2018-06-11 09:47:07 +0200

    msg

---
+++ Doc administratif/vue_mentions_legales.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>HomeNiscience</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="vue_mentions_legales.css" />
+</head>
+
+<body>
+<h4>
+Homeniscience est une PME composée exclusivement d’élèves de l’Institut Supérieur d’Electronique de Paris (ISEP). Elle a pour vocation de développer la domotique parmis les foyers parisiens en s’intéressant à une gestion poussée de la consommation énergétique des domiciles mais aussi d’exploitation d’équipements permettant de piloter son domicile.
+</h4>
+Adresse : 28 rue Notre Dame des Champs, 75006 <br>
+Téléphone : 01 49 54 52 00 <br>
+E-mail : accueil@homeniscience.com <br>
+N°SIRET : 446 115 305 111 30 <br>
+N°URSSAF : 076371435019112122 <br>
+N°TVA intracommunautaire : FR 69446116305 <br>
+Site Internet hébergé par Homeniscience. Le responsable du site Internet est le DSI d’Homeniscience. <br>
+<h4>
+Le Responsable du Traitement des Données d’Homeniscience est le DSI d’Homeniscience. <br>
+En application du Régime Général de Protection des Données (RGPD) effectif depuis le 25 Mai 2018, vous disposez d’un droit d’accès, de rectification, de portabilité, de suppression et d’opposition sur vos données.  <br>
+</h4>
+</body>
