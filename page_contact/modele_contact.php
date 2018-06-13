c4366f582e9e4057366e1fe909beb6e5a5a1a774
Victor Kolopp <vic.kolopp@gmail.com>
2018-06-13 09:52:47 +0200

    nidalee

--- page_contact/modele_contact.php
+++ page_contact/modele_contact.php
@@ -8,14 +8,7 @@ catch (Exception $e)
 {
     die('Erreur : ' . $e->getMessage());
 }
-//définition de la session de l'utilisateur
-$reponse = $bdd->query('SELECT ID, email, mot_de_passe,prenom,nom FROM utilisateur');
-while ($donnees = $reponse->fetch()) {
-    $_SESSION['ID']=$donnees['ID'];
-    $_SESSION['adresse']=$donnees['email'];
-    $_SESSION['prenom']=$donnees['prenom'];
-    $_SESSION['nom']=$donnees['nom'];
-}
+
 ?>
 
 
