43f5589f7ec3c52fa3097eb018f1925ed0bcd7ea
Guillaume Meynet <guillaume.meynet@isep.fr>
2018-06-15 11:26:14 +0200

    g

---
+++ Doc administratif/vue_rgpd.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>HomeNiscience</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="vue_mentions_legales.css" />
+</head>
+
+<body>
+
+<p>
+   Droit d'accès
+</p>
+<p>
+    Droit de rectification
+</p>
+<p>
+    Droit d'effacement
+</p>
+<p>
+    Droit de portabilité
+</p>
+<p>
+    Droit d'opposition
+</p>
+
+
+</body>
