6480b54ca6c0beaa5c664d4dbed7e8c7e37fc281
Ludovic <ludovic.martinet@isep.fr>
2018-06-26 00:06:48 +0100

    yo

--- page/gestionnaire/controller/appart_controller.php
+++ page/gestionnaire/controller/appart_controller.php
@@ -32,48 +32,65 @@ try {
     
     
     $capteur = getValeurCapteur(1, $_GET['id']);
+    $date_capteur = getDateCapteur(1, $_GET['id']);
     $array_date = array();
     $array_date_debut = array();
     $array_date_fin = array();
     
+    
+    while ($datum = $date_capteur -> fetch()) {
+        $date = explode(' ', $datum['date']);
+        array_push($array_date, $date[0]);
+    }
+    
+    
     $i = 0;
-   
     while ($donnees = $capteur -> fetch()) {
         
-        if ($donnees['donnee'] > 0050) {
-            $date = explode(' ', $donnees['date']);
-            array_push($array_date, $date[0]);
-            echo "bonjour";
+        if ($donnees['donnee'] > 0100) {
             if ($i == 0) {
-                echo "coucou";
-                array_push($array_date_debut, $date[0]);
+                array_push($array_date_debut, $donnees['date']);
                 $i = 1;
-                print_r($array_date_debut);
             }
         }
         else {
-            
-            $date = explode(' ', $donnees['date']);
-            array_push($array_date, $date[0]);
             if ($i == 1) {
-                array_push($array_date_fin, $date[0]);
+                array_push($array_date_fin, $donnees['date']);
                 $i = 0;
             }
         } 
     }
     
+    print_r($array_date);
+    print_r($array_date_debut);
+    print_r($array_date_fin);
+    
+    echo $array_date_debut[0];
+    
     $heure = array();
-    $date = array();
+    $debut = array();
+    $fin = array();
     $j = 0;
     while ($j <= count($array_date_debut) - 1) {
+        $date = explode(' ', $array_date_debut[$j]);
+        array_push($debut, $date[0]);
+        $date = explode(' ', $array_date_fin[$j]);
+        array_push($fin, $date[0]);
         
-        if ($array_date[$j] = $array_date[$j+1]) {
-            $heure[$j] +=  $array_date_fin[$j] - $array_date_debut[$j];
+        $date1 = date_create($array_date_debut[$j]);
+        $date2 = date_create($array_date_fin[$j]);
+        $diff = date_diff($date1, $date2);
+        
+        if ($debut[$j] = $fin[$j] && $j != 0 && $debut[$j] == $debut[$j-1]) {
+            $heure[$j-1] +=  $diff;
+        }
+        else if ($debut[$j] = $fin[$j]) {
+            $heure[$j] += $diff;
         }
         else {
-            $heure[$j+1] += $array_date_fin[$j] - $array_date_debut[$j];
+            $heure[$j+1] += $diff;
         }
-        $j += 1;
+        $j+=1;
     }
     
     $array_date = json_encode($array_date);
