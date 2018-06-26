9c925bd4b2064204483da05970b58cc48634a01c
Nicolas Le Borgne <nleborgne@isep.fr>
2018-06-26 11:56:14 +0200

    Changement CSS connexion admin

--- admin/vue/vue_connexion.php
+++ admin/vue/vue_connexion.php
@@ -5,7 +5,7 @@
     <title>Homeniscience Admin Board</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <!-- <link rel="stylesheet" type="text/css" media="screen and (max-width: 800px)" href="css/mobile.css" /> -->
-    <link rel="stylesheet" type="text/css" media="screen and (min-device-width: 801px)" href="/homeniscience/support/vue/style_connexion.css"/>
+    <link rel="stylesheet" type="text/css" media="screen and (min-device-width: 801px)" href="/homeniscience/admin/vue/style_connexion.css"/>
 </head>
 <body>
 <div class="login-page">
