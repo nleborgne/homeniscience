a5b6fe3e5c079a2e54ab4bc7ebb3fd996c18c535
Cyril Mazarin <cyrildu972@gmail.com>
2018-06-26 12:26:37 +0200

    commit

--- page/accueil/vue_domicile2.php
+++ page/accueil/vue_domicile2.php
@@ -1,17 +1,8 @@
 <!-- passer en mvc -->
 <table id="table" align="center">
 
-<<<<<<< HEAD
-<?php  if(strlen($domicile)<2){ ?>
-    <div class="container1" id="home" >
-        <div class="container1">
-            <div class="flex1" style="margin-left: auto; margin-right: auto">
-                <form method="post" action="index.php">
 
-                    <div class="panel" style="max-height: 500px; border: solid 1px #DDDDDD; text-align: center">
-                        <img src="warning.png" style="height: 250px;width: 250px;margin-top: 20px; margin-left: 250px; margin-right: 250px; position: center">
-                        <P style="padding-left: 100px;padding-right: 100px"> Ajoutez et configurez un domicile ! </P>
-=======
+
     <?php if (strlen($domicile) < 2) { ?>
         <div class="container1" id="home">
             <div class="container1">
@@ -23,7 +14,6 @@
                                  style="height: 250px;width: 250px;margin-top: 20px; margin-left: 250px; margin-right: 250px; position: center">
                             <P style="padding-left: 100px;padding-right: 100px"> Ajoutez et configurez un
                                 domicile!! </P>
->>>>>>> 031c4836fb300ed8627cb27afed0733854a5d2ae
 
                             <input class="boutton" type="submit" name="ajouterdomicile" value="mon domicile"
                                    style=" margin-left: 325px; margin-right: 325px; max-height: 40px; background-color: #ff9a00;  position: center">
